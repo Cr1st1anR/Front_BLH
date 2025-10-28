f01457f5ce63e478eb1f3585652290dd3f86d6f8
Cristhian Rengifo <cristhian860@hotmail.com>
2025-10-28 01:25:29 -0500

    chore: creación de estructura y carpetas del módulo frhos-063

---
+++ src/app/modules/captacion/frhos-063/components/entrega-leche-cruda-table/entrega-leche-cruda-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'entrega-leche-cruda-table',
+  imports: [],
+  templateUrl: './entrega-leche-cruda-table.component.html',
+  styleUrl: './entrega-leche-cruda-table.component.scss'
+})
+export class EntregaLecheCrudaTableComponent {
+
+}
---
+++ src/app/modules/captacion/frhos-063/pages/entrega-leche-cruda-page/entrega-leche-cruda-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'entrega-leche-cruda-page',
+  imports: [],
+  templateUrl: './entrega-leche-cruda-page.component.html',
+  styleUrl: './entrega-leche-cruda-page.component.scss'
+})
+export class EntregaLecheCrudaPageComponent {
+
+}

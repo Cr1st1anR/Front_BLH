9cacbe60cc59a3edc88a3529bedc2973cfe1e73b
Cristhian Rengifo <cristhian860@hotmail.com>
2025-10-29 10:57:47 -0500

    feat: boton nuevo registro

--- src/app/modules/captacion/frhos-063/components/new-register-button/new-register-button.component.ts
+++ src/app/modules/captacion/frhos-063/components/new-register-button/new-register-button.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'new-register-button-distribucion',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './new-register-button.component.html',
   styleUrl: './new-register-button.component.scss',
 })
-export class NewRegisterButtonComponent { }
+export class NewRegisterButtonComponent {
+  @Input() disabled = false;
+  @Output() nuevoRegistro = new EventEmitter<void>();
+
+  crearNuevoRegistro(): void {
+    this.nuevoRegistro.emit();
+  }
+}

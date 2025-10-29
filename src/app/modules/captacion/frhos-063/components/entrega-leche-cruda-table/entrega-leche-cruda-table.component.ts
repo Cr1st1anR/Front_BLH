d7564d50116778bc2dbe4ed7008c5e4b87b8befe
Cristhian Rengifo <cristhian860@hotmail.com>
2025-10-28 22:38:44 -0500

    feat: headers entrega leche cruda

--- src/app/modules/captacion/frhos-063/components/entrega-leche-cruda-table/entrega-leche-cruda-table.component.ts
+++ src/app/modules/captacion/frhos-063/components/entrega-leche-cruda-table/entrega-leche-cruda-table.component.ts
@@ -8,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class EntregaLecheCrudaTableComponent {
 
+  loading: boolean = false;
+
+  readonly headersEntregaLecheCruda = [
+    { header: 'FECHA', field: 'fecha', width: '50px', tipo: 'date' },
+    { header: 'NOMBRE DE LA MADRE', field: 'nombre_madre', width: '200px', tipo: 'text' },
+    { header: 'VOLUMEN DE LECHE MATERNA A.M', field: 'volumen_leche_materna_am', width: '150px', tipo: 'text' },
+    { header: 'VOLUMEN DE LECHE MATERNA P.M', field: 'volumen_leche_materna_pm', width: '150px', tipo: 'text' },
+    { header: 'PERDIDAS', field: 'perdidas', width: '80px', tipo: 'number' },
+    { header: 'RESPONSABLE', field: 'responsable', width: '100px', tipo: 'text' },
+  ];
+
+  dataEntregaLecheCruda: any[] = [];
+
+  
 }
--- src/app/modules/captacion/frhos-063/pages/entrega-leche-cruda-page/entrega-leche-cruda-page.component.ts
+++ src/app/modules/captacion/frhos-063/pages/entrega-leche-cruda-page/entrega-leche-cruda-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "src/app/shared/components/header/header.component";
 
 @Component({
   selector: 'entrega-leche-cruda-page',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './entrega-leche-cruda-page.component.html',
   styleUrl: './entrega-leche-cruda-page.component.scss'
 })

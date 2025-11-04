be417346b617f9d9237d5fd74ff614999ab979b9
Cristhian Rengifo <cristhian860@hotmail.com>
2025-11-03 21:29:59 -0500

    chore: nuevo componente pasterizacion-table

---
+++ src/app/modules/captacion/friam-032/components/pasterizacion-table/pasterizacion-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pasterizacion-table',
+  imports: [],
+  templateUrl: './pasterizacion-table.component.html',
+  styleUrl: './pasterizacion-table.component.scss'
+})
+export class PasterizacionTableComponent {
+
+}

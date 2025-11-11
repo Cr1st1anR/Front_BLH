8b26ca6f8e9c103fa4480f6fe779af4880d4972b
Cristhian Rengifo <cristhian860@hotmail.com>
2025-11-11 14:23:29 -0500

    feat: header friam-015

--- src/app/modules/pasteurizacion/friam-015/pages/seleccion-clasificacion-page/seleccion-clasificacion-page.component.ts
+++ src/app/modules/pasteurizacion/friam-015/pages/seleccion-clasificacion-page/seleccion-clasificacion-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "src/app/shared/components/header/header.component";
 
 @Component({
   selector: 'app-seleccion-clasificacion-page',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './seleccion-clasificacion-page.component.html',
   styleUrl: './seleccion-clasificacion-page.component.scss'
 })

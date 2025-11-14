9e1be510d9480a4eb0a15e9b0d2890897472c511
Cristhian Rengifo <cristhian860@hotmail.com>
2025-11-14 12:53:04 -0500

    chore: creación de estructura y carpetas del módulo friam-017

---
+++ src/app/modules/pasteurizacion/friam-017/components/no-conformidades-table/no-conformidades-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-conformidades-table',
+  imports: [],
+  templateUrl: './no-conformidades-table.component.html',
+  styleUrl: './no-conformidades-table.component.scss'
+})
+export class NoConformidadesTableComponent {
+
+}
---
+++ src/app/modules/pasteurizacion/friam-017/pages/no-conformidades-page/no-conformidades-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-conformidades-page',
+  imports: [],
+  templateUrl: './no-conformidades-page.component.html',
+  styleUrl: './no-conformidades-page.component.scss'
+})
+export class NoConformidadesPageComponent {
+
+}

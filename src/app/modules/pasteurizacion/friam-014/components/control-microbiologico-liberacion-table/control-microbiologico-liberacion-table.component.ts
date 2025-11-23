e8e898d8bd9995c47def0e35b9b5b4980b575f4b
Cristhian Rengifo <cristhian860@hotmail.com>
2025-11-23 15:18:55 -0500

    chore: creación de estructura y carpetas (friam-014)

---
+++ src/app/modules/pasteurizacion/friam-014/components/control-microbiologico-liberacion-table/control-microbiologico-liberacion-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-control-microbiologico-liberacion-table',
+  imports: [],
+  templateUrl: './control-microbiologico-liberacion-table.component.html',
+  styleUrl: './control-microbiologico-liberacion-table.component.scss'
+})
+export class ControlMicrobiologicoLiberacionTableComponent {
+
+}
---
+++ src/app/modules/pasteurizacion/friam-014/pages/control-microbiologico-liberacion-page/control-microbiologico-liberacion-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-control-microbiologico-liberacion-page',
+  imports: [],
+  templateUrl: './control-microbiologico-liberacion-page.component.html',
+  styleUrl: './control-microbiologico-liberacion-page.component.scss'
+})
+export class ControlMicrobiologicoLiberacionPageComponent {
+
+}

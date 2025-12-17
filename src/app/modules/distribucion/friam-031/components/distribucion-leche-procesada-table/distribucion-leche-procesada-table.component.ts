925a1623c30c4d638f373a86562ae86bcd6fdc91
Cristhian Rengifo <cristhian860@hotmail.com>
2025-12-17 12:55:10 -0500

    chore: creación de estructura y carpetas (friam-031)

---
+++ src/app/modules/distribucion/friam-031/components/distribucion-leche-procesada-table/distribucion-leche-procesada-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'distribucion-leche-procesada-table',
+  imports: [],
+  templateUrl: './distribucion-leche-procesada-table.component.html',
+  styleUrl: './distribucion-leche-procesada-table.component.scss'
+})
+export class DistribucionLecheProcesadaTableComponent {
+
+}
---
+++ src/app/modules/distribucion/friam-031/pages/distribucion-leche-procesada-page/distribucion-leche-procesada-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'distribucion-leche-procesada-page',
+  imports: [],
+  templateUrl: './distribucion-leche-procesada-page.component.html',
+  styleUrl: './distribucion-leche-procesada-page.component.scss'
+})
+export class DistribucionLecheProcesadaPageComponent {
+
+}

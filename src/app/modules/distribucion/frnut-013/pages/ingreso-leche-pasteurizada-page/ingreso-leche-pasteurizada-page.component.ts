bd7fc1e15ed622ed71718eb31145bb7c1180e87f
Cristhian Rengifo <cristhian860@hotmail.com>
2025-12-22 23:36:27 -0500

    feat: actualizar la página de ingreso de leche pasteurizada con el encabezado correspondiente

--- src/app/modules/distribucion/frnut-013/pages/ingreso-leche-pasteurizada-page/ingreso-leche-pasteurizada-page.component.ts
+++ src/app/modules/distribucion/frnut-013/pages/ingreso-leche-pasteurizada-page/ingreso-leche-pasteurizada-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "src/app/shared/components/header/header.component";
 
 @Component({
-  selector: 'app-ingreso-leche-pasteurizada-page',
-  imports: [],
+  selector: 'ingreso-leche-pasteurizada-page',
+  imports: [HeaderComponent],
   templateUrl: './ingreso-leche-pasteurizada-page.component.html',
   styleUrl: './ingreso-leche-pasteurizada-page.component.scss'
 })

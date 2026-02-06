7d3259b3a85a7ce142e0b039ccee71c4a83afce0
Cristhian Rengifo <cristhian860@hotmail.com>
2026-02-06 16:39:02 -0500

    feat: eliminar subetiquetas de los elementos del menú en el dashboard

--- src/app/shared/components/dash-board/dash-board.component.ts
+++ src/app/shared/components/dash-board/dash-board.component.ts
@@ -53,42 +53,34 @@ export class DashBoardComponent {
       items: [
         {
           label: 'Registro de linea amiga',
-          subLabel: '(FRIAM-041)',
           route: '/blh/captacion/registro-linea-amiga',
         },
         {
           label: 'Ruta de recolección de leche humana cruda',
-          subLabel: '(FRIAM-011)',
           route: '/blh/captacion/recoleccion-leche-humana-cruda',
         },
         {
           label: 'Registro de donante del banco de leche humana',
-          subLabel: '(FRIAM-018)',
           route: '/blh/captacion/registro-donante-blh',
         },
         {
           label: 'Visita domiciliaria de ingreso para madres donantes del programa blh',
-          subLabel: '(FRIAM-037)',
           route: '/blh/captacion/visita-domiciliaria',
         },
         {
           label: 'Visitas domiciliarias de seguimiento para madres donantes del programa blh',
-          subLabel: '(FRIAM-038)',
           route: '/blh/captacion/visitas-domiciliarias-seguimiento',
         },
         {
           label: 'Control de entradas y salidas de leche humana extraida cruda',
-          subLabel: '(FRIAM-012)',
           route: '/blh/captacion/control-leche-cruda'
         },
         {
           label: 'Registro de leche materna extraida en sala de extracción de blh',
-          subLabel: '(FRIAM-016)',
           route: '/blh/captacion/registro-leche-extraida',
         },
         {
           label: 'Entrega de leche humana cruda a sala de distribución blh',
-          subLabel: '(FRHOS-063)',
           route: '/blh/captacion/entrega-leche-cruda',
         },
       ],
@@ -100,27 +92,22 @@ export class DashBoardComponent {
       items: [
         {
           label: 'Control de reenvase red colombiana de bancos de leche humana',
-          subLabel: '(FRIAM-032)',
           route: '/blh/pasteurizacion/control-reenvase',
         },
         {
           label: 'Seleccion y clasificacion de leche humana extraida cruda',
-          subLabel: '(FRIAM-015)',
           route: '/blh/pasteurizacion/seleccion-clasificacion-leche-cruda',
         },
         {
           label: 'Control de temperatura del pasteurizador blh',
-          subLabel: '(FRIAM-036)',
           route: '/blh/pasteurizacion/control-temperatura-pasteurizador',
         },
         {
           label: 'Registro diario de no conformidades',
-          subLabel: '(FRIAM-017)',
           route: '/blh/pasteurizacion/registro-no-conformidades',
         },
         {
           label: 'Relación de control de calidad microbiologico y liberacion de producto',
-          subLabel: '(FRIAM-014)',
           route: '/blh/pasteurizacion/control-calidad-microbiologico-liberacion',
         },
       ],
@@ -132,7 +119,6 @@ export class DashBoardComponent {
       items: [
         {
           label: 'Control de entradas y salidas de leche humana extraida pasteurizada',
-          subLabel: '(FRIAM-013)',
           route: '/blh/liberacion/entradas-salidas-pasteurizada',
         },
       ],
@@ -144,12 +130,10 @@ export class DashBoardComponent {
       items: [
         {
           label: 'Distribución de leche humana procesada blh',
-          subLabel: '(FRIAM-031)',
           route: '/blh/distribucion/distribucion-leche-procesada',
         },
         {
           label: 'Registro de ingreso de leche humana pasteurizada',
-          subLabel: '(FRNUT-013)',
           route: '/blh/distribucion/ingreso-leche-pasteurizada',
         },
       ],
@@ -161,7 +145,6 @@ export class DashBoardComponent {
       items: [
         {
           label: 'Construcción de curvas de penetración de calor y enfriamiento',
-          subLabel: '(FRIAM-019)',
           route: '/blh/curvas/construccion-curvas',
         },
       ],
